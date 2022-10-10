1d2a1b42de8a56806d3cf91e0f3955a5c3789f58
ncomerci <ncomerci@itba.edu.ar>
2022-10-10 21:44:02 +0200

    filters started

--- src/components/PartnersList/PartnersList.tsx
+++ src/components/PartnersList/PartnersList.tsx
@@ -2,38 +2,80 @@ import React, { useState, useEffect, useMemo } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { PaymentMethod, Region, Service, TeamSize, VerifiedPartner } from '../../interfaces/VerifiedPartner'
 import PartnerCard from '../PartnerCard/PartnerCard'
-import { Dropdown, AccordionTitleProps, Accordion, Form, Menu } from 'semantic-ui-react'
+import { Dropdown, AccordionTitleProps, Accordion, Form, Menu, CheckboxProps, Button } from 'semantic-ui-react'
 interface Props {
   partners: VerifiedPartner[]
 }
 
+enum FilterType {
+  Services = 'services',
+  Region = 'region',
+  TeamSize = 'team_size',
+  PaymentMethod = 'payment_methods',
+  Language = 'languages',
+}
+
+type Filters = Record<FilterType, string[]>
+const EMPTY_FILTER = {} as Filters
+
 const dropdownContent = [
   {
-    title: <FormattedMessage id="services" key="services" />,
+    key: FilterType.Services,
+    title: <FormattedMessage id={FilterType.Services} />,
     options: Service,
   },
   {
-    title: <FormattedMessage id="region" key="region" />,
+    key: FilterType.Region,
+    title: <FormattedMessage id={FilterType.Region} />,
     options: Region,
   },
   {
-    title: <FormattedMessage id="team_size" key="team_size" />,
+    key: FilterType.TeamSize,
+    title: <FormattedMessage id={FilterType.TeamSize} />,
     options: TeamSize,
   },
   {
-    title: <FormattedMessage id="payment_methods" key="payment_methods" />,
+    key: FilterType.PaymentMethod,
+    title: <FormattedMessage id={FilterType.PaymentMethod} />,
     options: PaymentMethod,
   },
 ]
 
 function PartnersList({ partners }: Props) {
-  const [idx, setIdx] = useState(0)
+  const [currentIdx, setCurrentIdx] = useState(0)
+  const [filters, setFilters] = useState<Filters>(EMPTY_FILTER)
+  const [filteredPartners, setFilteredPartners] = useState(partners)
 
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, titleProps: AccordionTitleProps) => {
+  const handleTitleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, titleProps: AccordionTitleProps) => {
     e.stopPropagation()
     const { index } = titleProps
-    setIdx(Number(index))
-    console.log(idx)
+    const idxNumber = Number(index)
+    if (currentIdx === idxNumber) {
+      setCurrentIdx(-1)
+    } else {
+      setCurrentIdx(idxNumber)
+    }
+  }
+
+  const handleItemClick = (event: React.MouseEvent<HTMLInputElement, MouseEvent>, itemData: CheckboxProps) => {
+    event.stopPropagation()
+    const { checked, name, value } = itemData
+    const filterType = name as FilterType
+
+    if (checked) {
+      setFilters((prev) => ({ ...prev, [filterType]: [...(prev[filterType] || []), value] }))
+    } else {
+      const newFilters = filters[filterType].filter((item) => item !== value)
+      if (newFilters.length === 0) {
+        setFilters((prev) => {
+          const filterRemoved = { ...prev }
+          delete filterRemoved[filterType]
+          return filterRemoved
+        })
+      } else {
+        setFilters((prev) => ({ ...prev, [filterType]: newFilters }))
+      }
+    }
   }
 
   const languages = useMemo(() => {
@@ -41,6 +83,30 @@ function PartnersList({ partners }: Props) {
     partners.map((partner) => (uniqueLanguages = new Set([...uniqueLanguages, ...partner.languages])))
     return Array.from(uniqueLanguages).sort((a, b) => a.localeCompare(b))
   }, [partners])
+
+  const applyFilters = () => {
+    const selectedPartners = partners.filter((partner) =>
+      Object.entries(filters).every(([type, filters]) => {
+        const filterKey = type as `${FilterType}`
+        return filters.some((filter) => partner[filterKey] === filter || partner[filterKey].includes(filter as never))
+      })
+    )
+
+    if (Object.keys(selectedPartners).length === 0 && Object.keys(filters).length === 0) {
+      setFilteredPartners(partners)
+    } else {
+      setFilteredPartners(selectedPartners)
+    }
+  }
+
+  const clearFilters = () => {
+    setFilters(EMPTY_FILTER)
+    setFilteredPartners(partners)
+  }
+
+  useEffect(() => {
+    console.log(filters)
+  }, [filters])
 
   return (
     <>
@@ -52,20 +118,25 @@ function PartnersList({ partners }: Props) {
           <Accordion>
             {dropdownContent.map((item, index) => {
               return (
-                <Menu.Item key={item.title.key}>
-                  <Accordion.Title active={idx === index} content={item.title} index={index} onClick={handleClick} />
+                <Menu.Item key={item.key}>
+                  <Accordion.Title
+                    active={currentIdx === index}
+                    content={item.title}
+                    index={index}
+                    onClick={handleTitleClick}
+                  />
                   <Accordion.Content
-                    active={idx === index}
+                    active={currentIdx === index}
                     content={
                       <Form>
                         <Form.Group grouped>
                           {Object.entries(item.options).map(([key, value]) => (
                             <Form.Checkbox
                               label={value}
-                              name={key}
-                              value={key}
+                              name={item.key}
+                              value={value}
                               key={key}
-                              onClick={(e) => e.stopPropagation()}
+                              onClick={handleItemClick}
                             />
                           ))}
                         </Form.Group>
@@ -77,23 +148,23 @@ function PartnersList({ partners }: Props) {
             })}
             <Menu.Item>
               <Accordion.Title
-                active={idx === dropdownContent.length + 1}
+                active={currentIdx === dropdownContent.length + 1}
                 content={<FormattedMessage id="languages" />}
                 index={dropdownContent.length + 1}
-                onClick={handleClick}
+                onClick={handleTitleClick}
               />
               <Accordion.Content
-                active={idx === dropdownContent.length + 1}
+                active={currentIdx === dropdownContent.length + 1}
                 content={
                   <Form>
                     <Form.Group grouped>
                       {languages.map((language) => (
                         <Form.Checkbox
                           label={language}
-                          name={language}
+                          name={FilterType.Language}
                           value={language}
                           key={language}
-                          onClick={(e) => e.stopPropagation()}
+                          onClick={handleItemClick}
                         />
                       ))}
                     </Form.Group>
@@ -102,9 +173,17 @@ function PartnersList({ partners }: Props) {
               />
             </Menu.Item>
           </Accordion>
+          <div>
+            <Button onClick={clearFilters}>
+              <FormattedMessage id="clear" />
+            </Button>
+            <Button onClick={applyFilters}>
+              <FormattedMessage id="apply" />
+            </Button>
+          </div>
         </Dropdown.Menu>
       </Dropdown>
-      {partners.map((partner) => (
+      {filteredPartners.map((partner) => (
         <PartnerCard key={partner.id} partner={partner} />
       ))}
     </>

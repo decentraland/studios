ab4d87c3587845d76bb62034bc323039c5cfbc15
ncomerci <ncomerci@itba.edu.ar>
2022-10-17 16:47:07 +0200

    fix: pagination fetch

--- src/pages/api/partners.ts
+++ src/pages/api/partners.ts
@@ -3,11 +3,27 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 const VERIFIED_PARTNERS_URL = `${process.env.NEXT_PUBLIC_PARTNERS_DATA_URL}/items/profile`
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<VerifiedPartner | { error: unknown }>) {
-  try {
-    const response = await fetch(VERIFIED_PARTNERS_URL)
-    res.status(200).json((await response.json()).data)
-  } catch (error) {
-    res.status(500).json({ error })
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<VerifiedPartner[] | { error: unknown }>
+) {
+  let isFinished = false
+  let offset = 0
+  const partners: VerifiedPartner[] = []
+  while (!isFinished) {
+    try {
+      const response = await fetch(`${VERIFIED_PARTNERS_URL}?offset=${offset}`)
+      const data = (await response.json()).data as VerifiedPartner[]
+      if (data.length === 0) {
+        isFinished = true
+      } else {
+        offset += data.length
+        partners.push(...data)
+      }
+    } catch (error) {
+      return res.status(500).json({ error })
+    }
   }
+
+  return res.status(200).json(partners)
 }

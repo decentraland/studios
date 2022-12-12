7490c38e67dbfe5ecae03b937fab5bc1563214ce
Pejosonic <bobrow@gmail.com>
2022-12-12 14:48:06 -0300

    feat: api redirect to WEBSITE_URL var

--- src/pages/api/verify/[uuid].ts
+++ src/pages/api/verify/[uuid].ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const VERIFY_API_URL = process.env.VERIFY_API
+const WEBSITE_URL = process.env.WEBSITE_URL
 
 type ResponseError = {
   message: string
@@ -17,5 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const verifyRes = await fetch(`${VERIFY_API_URL}?rev=${uuid}`)
   const status = await verifyRes.json()
 
-  return status.name === 'error' ? res.redirect('/verify/error') : res.redirect('/verify/success')
+  return status.name === 'error'
+    ? res.redirect(`${WEBSITE_URL}/verify/error`)
+    : res.redirect(`${WEBSITE_URL}/verify/success`)
 }

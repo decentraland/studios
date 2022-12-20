46453139ae4e4ad85190fe64eb4fbda2558e3963
Francisco Crespi <franciscocrespi@Franciscos-MBP.home>
2022-12-20 11:39:31 +0100

    adjust the report studio button

--- src/components/PartnerProfile/PartnerProfile.tsx
+++ src/components/PartnerProfile/PartnerProfile.tsx
@@ -147,9 +147,11 @@ function PartnerProfile({ partner, projects }: Props) {
           </div>
         )}
       </div>
-      <a href="https://dclstudios.typeform.com/to/HQpD0z5S" target="_blank" rel="noreferrer">
-        Report this studio
-      </a>
+      <div className={styles.report_link}>
+        <a href="https://dclstudios.typeform.com/to/HQpD0z5S" target="_blank" rel="noreferrer">
+          Report this studio
+        </a>
+      </div>
     </div>
   )
 }

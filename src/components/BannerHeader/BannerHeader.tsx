45ab64d1f4315c3b01cae2f817d898e0f3b10a49
Pejosonic <bobrow@gmail.com>
2022-12-27 20:13:38 -0300

    feat: landing page banner header

---
+++ src/components/BannerHeader/BannerHeader.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './BannerHeader.module.css'
+
+function BackButton() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftPanel}>
+        <div className={styles.mainText}>Your brand belongs in the Metaverse. Let’s get you a team to build with.</div>
+        <div className={styles.factsContainer}>
+          <div className={styles.factContainer}>
+            <div className={styles.bigText}>180+</div>
+            <div className={styles.smallText}>VERFIED STUDIOS</div>
+          </div>
+          <div className={styles.factContainer}>
+            <div className={styles.bigText}>200+</div>
+            <div className={styles.smallText}>PROJECTS COMPLETED</div>
+          </div>
+        </div>
+      </div>
+      <img className={styles.image} src="/images/computer.png"></img>
+    </div>
+  )
+}
+
+export default BackButton

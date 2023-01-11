31be69478824124d99b15abb15003c7d178440a8
Pejosonic <bobrow@gmail.com>
2023-01-11 02:16:25 -0300

    feat: Intercom lazy load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,10 +43,13 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="twitter:card" content="summary" />
         <meta name="og:image" content="/images/decentraland.png" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>      
-      {loadIntercom({
-        appId: INTERCOM_APP_ID,
-      })}
+      </Head>
+      <Script id='intercom' >{`(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/${INTERCOM_APP_ID}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+
+// Call boot method
+(window).Intercom('boot', {
+app_id: '${INTERCOM_APP_ID}'
+});`}</Script>
       <Script id="metabase">
         {`!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${METABASE_KEY}";;analytics.SNIPPET_VERSION="4.15.3";
           analytics.load("${METABASE_KEY}");

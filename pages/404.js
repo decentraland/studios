9e0899d645db521eb9125b2af3035ff198c4d5df
Pejosonic <bobrow@gmail.com>
2023-01-17 16:42:20 -0300

    feat: custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+
+export default function Page() {
+    return (
+    <div style={{textAlign: 'center', height:'calc(100vh - 300px)'}}>
+        <div style={{position: 'relative', top: '50%'}}>
+            <h1 >404</h1>
+            <div >This page could not be found</div>
+        </div>
+    </div>)
+    }

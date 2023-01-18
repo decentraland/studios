71900685bc4db940ddc55e94017d0ff61dc19c54
Pejosonic <bobrow@gmail.com>
2023-01-18 18:09:21 -0300

    feat: filters services icons

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -188,7 +188,7 @@ function Filters({ partners, setFilteredPartners }: Props) {
                 let circleDiv = null
                 let serviceTooltipDiv = null
                 if (itemData.name === 'services') {
-                  circleDiv = <div className={styles.circle} />
+                  circleDiv = <img className={styles.circle} src={`/images/category_${key}.webp`} />
                   serviceTooltipDiv = (
                     <div className={styles.tooltip_container}>
                       <div className={styles.tooltip}>

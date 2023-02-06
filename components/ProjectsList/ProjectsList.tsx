bdaf15185a4a430cc9720c8bec379b117edbc328
Pejosonic <bobrow@gmail.com>
2023-02-06 16:46:26 -0300

    feat: pagination on list pages

--- components/ProjectsList/ProjectsList.tsx
+++ components/ProjectsList/ProjectsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectCard from '../ProjectCard/ProjectCard'
 import { PartnerProjectExtended } from '../../interfaces/PartnerProject'
 import styles from './ProjectsList.module.css'
@@ -6,11 +6,17 @@ import styles from './ProjectsList.module.css'
 
 function ProjectsList({ projects }: { projects: PartnerProjectExtended[] }) {
     // const renderProjects = projects.sort((a,b) => new Date(b.date_updated || b.date_created).getTime()  - new Date(a.date_updated || a.date_created).getTime())
+    const [limit, setLimit] = useState(18)
+
+    const renderProjects = projects.slice(0, limit)
 
     return(
-      <div className={styles.projects_grid}>
-          {projects.map((project) => <ProjectCard key={project.id} project={project} />)}
-      </div>
+      <>
+        <div className={styles.projects_grid}>
+            {renderProjects.map((project) => <ProjectCard key={project.id} project={project} />)}
+        </div>
+        {projects.length >= limit && <div onClick={() => setLimit(current => current + 12)} className='button_primary center'>LOAD MORE</div>}
+      </>
     )
 
 }

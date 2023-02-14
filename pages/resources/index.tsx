bfd6f997b63e3a5b7e91b87baf959087c61744e6
Pejosonic <bobrow@gmail.com>
2023-02-13 22:21:10 -0300

    feat: persistant show count on lists pages

--- pages/resources/index.tsx
+++ pages/resources/index.tsx
@@ -1,6 +1,6 @@
 import styles from '../../styles/Home.module.css'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 
 import { Container } from 'decentraland-ui/dist/components/Container/Container'

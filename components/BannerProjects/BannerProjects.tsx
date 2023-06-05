d9692c4c01dbd745684aecc5c2f3adec74e83423
Pejosonic <bobrow@gmail.com>
2023-06-05 18:58:29 -0300

    feat: UI fixes

--- components/BannerProjects/BannerProjects.tsx
+++ components/BannerProjects/BannerProjects.tsx
@@ -6,7 +6,7 @@ function BannerProjects() {
     <div className={styles.container}>
       <div className={styles.leftPanel}>
         <div className={styles.mainText}>Browse the latest projects on Decentraland and get new ideas for your brand.</div>
-        <div className={styles.smallText}>Hundreds of designers and studios around the world showcase their portfolio work on Metaverse Studios.</div>
+        <div className={styles.smallText}>Hundreds of designers and studios around the world showcase their portfolio work on Decentraland Studios.</div>
       </div>
       <img alt='Browse the latest projects on Decentraland and get new ideas for your brand' className={styles.image} src="/images/banner_projects.webp"></img>
     </div>

d5d4bd48c23ec8c6f5fb69e21c25028c8308ee9c
Pejosonic <bobrow@gmail.com>
2023-07-03 13:24:16 +0100

    feat: sitemap.xml add lastmod prop

--- clients/sitemap.js
+++ clients/sitemap.js
@@ -1,29 +1,35 @@
 
 
 
-function generateSiteMap(paths) {
+function generateSiteMap(partnersPaths, projectsPaths, guidesPaths, resourcesPaths) {
     return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
        <loc>https://studios.decentraland.org</loc>
+       <lastmod>2023-06-01</lastmod>
      </url>
      <url>
        <loc>https://studios.decentraland.org/jobs</loc>
+       <lastmod>2023-06-01</lastmod>
      </url>
      <url>
        <loc>https://studios.decentraland.org/studios</loc>
+       <lastmod>${partnersPaths[0][1] || partnersPaths[0][2]}</lastmod>
      </url>
      <url>
        <loc>https://studios.decentraland.org/projects</loc>
+       <lastmod>${projectsPaths[0][1] || projectsPaths[0][2]}</lastmod>
      </url>
      <url>
        <loc>https://studios.decentraland.org/resources</loc>
+       <lastmod>${resourcesPaths[0][1] || resourcesPaths[0][2]}</lastmod>
      </url>
-     ${paths
-            .map((path) => {
+     ${[ ...partnersPaths, ...projectsPaths, ...guidesPaths]
+            .map(([path, date_updated, date_created]) => {
                 return `
        <url>
-           <loc>${`https://studios.decentraland.org/${path}`}</loc>
+           <loc>https://studios.decentraland.org/${path}</loc>
+           <lastmod>${date_updated || date_created}</lastmod>
        </url>
      `;
             })
@@ -33,7 +39,6 @@ function generateSiteMap(paths) {
 }
 
 async function writeSitemap() {
-  //TODO: make dynamic site sections, handle db fetch errors?
 
     console.log('Generating public/sitemap.xml')
 
@@ -41,14 +46,15 @@ async function writeSitemap() {
 
     const DB_URL = `${process.env.NEXT_PUBLIC_PARTNERS_DATA_URL}`
 
-    const partnersPaths = (await (await fetch(`${DB_URL}/items/profile?fields=slug&limit=-1`)).json()).data.map((item) => `profile/${item.slug}`)
+    const partnersPaths = (await (await fetch(`${DB_URL}/items/profile?fields=slug,date_updated,date_created&limit=-1&sort=-date_created,-date_updated`)).json()).data.map((item) => [`profile/${item.slug}`, item.date_updated, item.date_created])
 
-    const projectsPaths = (await (await fetch(`${DB_URL}/items/projects?fields=id&limit=-1`)).json()).data.map((item) => `project/${item.id}`)
-
-    const guidesPaths = (await (await fetch(`${DB_URL}/items/landings?fields=slug&limit=-1`)).json()).data.map((item) => `p/${item.slug}`)
-
-    const sitemap = generateSiteMap([...partnersPaths, ...projectsPaths, ...guidesPaths]);
-
+    const projectsPaths = (await (await fetch(`${DB_URL}/items/projects?fields=id,date_updated,date_created&limit=-1&sort=-date_created,-date_updated`)).json()).data.map((item) => [`project/${item.id}`, item.date_updated, item.date_created])
+    
+    const guidesPaths = (await (await fetch(`${DB_URL}/items/landings?fields=slug,date_updated,date_created&limit=-1&sort=-date_created,-date_updated`)).json()).data.map((item) => [`p/${item.slug}`, item.date_updated, item.date_created])
+    
+    const resourcesPaths = (await (await fetch(`${DB_URL}/items/resources?fields=id,date_updated,date_created&limit=1&sort=-date_created,-date_updated`)).json()).data.map((item) => [item.id, item.date_updated, item.date_created])
+    
+    const sitemap = generateSiteMap(partnersPaths, projectsPaths, guidesPaths, resourcesPaths);
 
     fs.writeFileSync('public/sitemap.xml', sitemap)
 }

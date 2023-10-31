abbbb1cbbfa45dfa59a12b94ce502bee2e975ee3
Pejosonic <bobrow@gmail.com>
2023-10-31 12:16:28 -0500

    feat: securty fixes for Acunetix

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse, userAgent } from 'next/server'
+
+export const config = {
+  matcher: '/api/:path*',
+}
+
+export function middleware(req: NextRequest) {
+  const { ua } = userAgent(req)
+
+  if (ua.includes('Acunetix')) return new Response(null, { status: 401 })
+
+  return NextResponse.rewrite(req.nextUrl)
+}
